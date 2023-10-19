e0287b12222d74e86537ef3ddc3f2895810c4ebd
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-10-19 15:01:25 +1300

    add deployed url to websocket

--- client/components/Pages/Home.tsx
+++ client/components/Pages/Home.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost:3000 || ', {
+export const socket = io('https://messaging-app-websockets.up.railway.app/', {
   transports: ['websocket'],
 })
 
